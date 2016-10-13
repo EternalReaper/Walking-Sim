78ce958e330dc4d6ee1f2bb7b7469bc6e8f9b242
EternalReaper <henry.sandersberg@yahoo.com.au>
2016-10-13 20:58:20 +1100

    added a countdown timer
    remind me of this and ill explain my idea for it, if my idea doesnt
    float then we can change it to a regular game timer

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour {
+
+	public Text m_TimerText;
+	private float m_gameTime = 1200;
+
+	public float GameTime { get { return m_gameTime; } }
+	void Start () {
+		m_TimerText.gameObject.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_gameTime -= Time.deltaTime;
+		if (m_gameTime < 0)
+		{
+			m_gameTime = 0;
+		}
+		int seconds = Mathf.RoundToInt(m_gameTime);
+		m_TimerText.text = string.Format("{0:D2}:{1:D2}",
+			(seconds / 60), 
+			(seconds % 60));
+		
+	}
+}
