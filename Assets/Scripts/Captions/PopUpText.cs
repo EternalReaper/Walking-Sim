387f80990e6b94356abc22630b613ebb730e90b7
Stormdrigger <liamkeogh2000@gmail.com>
2016-10-25 18:58:12 +1100

    Subtitles And Triggers
    This Includes the script for subtitles and also some of the triggers

---
+++ Assets/Scripts/Captions/PopUpText.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class PopUpText : MonoBehaviour
+{
+    [SerializeField]
+    private Text TargetText;
+    [SerializeField]
+    private string ChangeME;
+    [SerializeField]
+    private bool Activated;
+    [SerializeField]
+    private float TimeActive;
+
+    void OnTriggerEnter(Collider WhoHitMe)
+    {
+        if (WhoHitMe.gameObject.CompareTag("Player"))
+        {
+            Activated = true;
+            TargetText.text = ChangeME;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Activated)
+        {
+            TargetText.gameObject.SetActive(true);
+            if (TimeActive > 0f)
+            {
+                TimeActive -= Time.deltaTime;
+
+            }
+            else
+            {
+                Activated = false;
+                TargetText.gameObject.SetActive(false);
+            }
+        }
+    }
+}
