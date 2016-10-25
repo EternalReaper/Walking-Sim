d144a4cf9c8ce711ea8732c8ce01f94977f293e8
Fireball0299 <antsnz@yahoo.com>
2016-10-25 20:55:14 +1100

    Finished Main Menu and Credits

---
+++ Assets/Scripts/Main Menu/ButtonLoadScene.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonLoadScene : MonoBehaviour
+{
+    public string _sceneName = string.Empty;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnButtonPressed()
+    {
+        Application.LoadLevel(_sceneName);
+	}
+}
